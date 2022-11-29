72f9a76705bb811c1b131c3b3c9511cba8da5a6b
vamasc <vasecmys@gmail.com>
2022-11-30 00:22:18 +0200

    add JavaDoc

---
+++ JavaDoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.company"},{"l":"com.company.bl"},{"l":"com.company.dao"},{"l":"com.company.Interface"}];updateSearchResults();
